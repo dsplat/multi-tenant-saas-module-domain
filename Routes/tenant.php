09870cea3e6988fe68a5a3487d0ef530d4368506
luoyueliang <luoyueliang@gmail.com>
2026-07-20 18:12:07 +0800

    fix(auth): resolve tenant from domain query param in OAuth redirect/callback

--- Routes/tenant.php
+++ Routes/tenant.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use MultiTenantSaas\Modules\Domain\Http\Controllers\TenantDomainController;
 
-Route::prefix('tenant/domain')->group(function () {
+Route::prefix('tenant/{tenantId}/domain')->group(function () {
     Route::get('/', [TenantDomainController::class, 'index']);
     Route::post('/', [TenantDomainController::class, 'store']);
     Route::put('/', [TenantDomainController::class, 'update']);
